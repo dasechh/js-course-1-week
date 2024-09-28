dd52029c126a70c5bf9df220c69e9d1c006b2376
ondryusha <mac@MacBook-Pro--ondryusha.local>
2024-09-28 12:04:29 +0300

    code fixes

--- Task-3/js/main.js
+++ Task-3/js/main.js
@@ -1,7 +1,11 @@
 const main = () => { // Объявление функции main
   let p = document.getElementById("ico-volume"); // Обозначение тега p по его айди
   let lenght = document.getElementById("length").value; // Получение значения из input с id 'length'
-  return (p.innerHTML = ((5 * lenght ** 3) / 12) * (3 + Math.sqrt(5))); // Изменение содержимого тега p, расчёт объёма икосаэдра
+  return (p.innerHTML = count(lenght)); // Изменение содержимого тега p
 };
+
+const count = (lenght) => { // функция для рассчета объема икосаэдра
+  return (5 * lenght ** 3) / 12 * (3 + Math.sqrt(5));
+}
 
 document.getElementById("submit").addEventListener("click", main); // Ожидание нажатия кнопки, при нажатии запуск функции main
